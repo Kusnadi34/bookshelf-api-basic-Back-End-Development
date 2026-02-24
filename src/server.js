313757026c03e54630c803d31c81c4d0580b74a1
Kusnadi34 <nanakusnadi035@gmail.com>
2026-02-24 11:48:53 +0700

    "Upload server.js sebagai entry point aplikasi"
    · "File ini adalah entry point utama aplikasi Bookshelf API. Menggunakan Express dan mendefinisikan route /books."

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-add server.js
+const express = require('express');
+const booksRoutes = require('./routes/books');
+
+const app = express();
+const PORT = 9000;
+
+app.use(express.json());
+app.use('/books', booksRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
+});
